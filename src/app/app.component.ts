4249cafd71fdbd52dee092c39f0cc325b980b2c7
Justin Sin <48116685+jvil26@users.noreply.github.com>
2020-07-08 18:45:55 -0700

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
+import "@alan-ai/alan-button";
 
-import { MenuController, Platform, ToastController } from '@ionic/angular';
+import { AlertController, MenuController, Platform, ToastController, NavController } from '@ionic/angular';
 
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -10,12 +11,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Storage } from '@ionic/storage';
 
 import { UserData } from './providers/user-data';
+import { ConferenceData } from './providers/conference-data'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   appPages = [
@@ -42,17 +43,22 @@ export class AppComponent implements OnInit {
   ];
   loggedIn = false;
   dark = false;
+  @ViewChild('alanBtnEl', {static:false}) alanBtnComponent: ElementRef<HTMLAlanButtonElement>;
 
   constructor(
+    private alertCtrl: AlertController,
     private menu: MenuController,
     private platform: Platform,
     private router: Router,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private storage: Storage,
+    private user: UserData,
     private userData: UserData,
     private swUpdate: SwUpdate,
     private toastCtrl: ToastController,
+    private confData: ConferenceData,
+    private navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -80,8 +86,123 @@ export class AppComponent implements OnInit {
         .then(() => this.swUpdate.activateUpdate())
         .then(() => window.location.reload());
     });
+
+  }
+  async addFavorite(sessionData: any) {
+    this.user.addFavorite(sessionData.name);
+    this.alanBtnComponent.nativeElement.setVisualState({favName: sessionData.name});
+    // Create a toast
+      const toast = await this.toastCtrl.create({
+        header: `${sessionData.name} was successfully added as a favorite.`,
+        duration: 3000,
+        buttons: [{
+          text: 'Close',
+          role: 'cancel'
+        }]
+      });
+
+      // Present the toast at the bottom of the page
+      await toast.present();
+    }
+
+
+
+  async removeFavorite(sessionData: any) {
+    this.user.removeFavorite(sessionData.name);
+    this.alanBtnComponent.nativeElement.setVisualState({removeFavName: sessionData.name});
+    const toast = await this.toastCtrl.create({
+      header: `${sessionData.name} was successfully added as a favorite.`,
+      duration: 3000,
+      buttons: [{
+        text: 'Close',
+        role: 'cancel'
+      }]
+    });
+    // now present the alert on top of all other content
+    await toast.present();
   }
 
+  ngAfterViewInit() {
+    this.alanBtnComponent.nativeElement.addEventListener('command', (data) => {
+        const commandData = (<CustomEvent>data).detail;
+
+        if (commandData.command === 'navigation') {
+            //call client code that will react to the received command
+            this.navCtrl.navigateForward([commandData.route]);
+        }
+
+        if(commandData.command === 'scheduleNavigation'){
+          this.navCtrl.navigateForward([commandData.agendaRoute]);
+        }
+
+      let agendaIndex;
+        if(commandData.command === 'readAgenda'){
+          console.log(commandData.id);
+          for(let i in this.confData.data.schedule[0].groups){
+              agendaIndex = this.confData.data.schedule[0].groups[i].sessions.findIndex(x => x.id === `${commandData.id}`);
+              console.log(agendaIndex);
+              if(agendaIndex > -1){
+                this.alanBtnComponent.nativeElement.setVisualState({agenda: this.confData.data.schedule[0].groups[i].sessions[agendaIndex]});
+                break;
+              }
+          }
+          if(agendaIndex < 0){
+            this.alanBtnComponent.nativeElement.setVisualState({agenda: ''});
+          }
+        }
+
+        let favIndex;
+        if(commandData.command === 'addFavorite'){
+          for(let i in this.confData.data.schedule[0].groups){
+              favIndex = this.confData.data.schedule[0].groups[i].sessions.findIndex(x => x.id === `${commandData.id}`);
+              console.log(favIndex);
+              if(favIndex > -1){
+                this.addFavorite(this.confData.data.schedule[0].groups[i].sessions[favIndex]);
+                break;
+              }
+          }
+          if(favIndex < 0){
+            this.alanBtnComponent.nativeElement.setVisualState({favName: ''});
+          }
+        }
+
+        let removeFavIndex;
+        if(commandData.command === 'removeFavorite'){
+          for(let i in this.confData.data.schedule[0].groups){
+            removeFavIndex = this.confData.data.schedule[0].groups[i].sessions.findIndex(x => x.id === `${commandData.removeID}`);
+              console.log(removeFavIndex);
+              if(removeFavIndex > -1){
+                this.removeFavorite(this.confData.data.schedule[0].groups[i].sessions[removeFavIndex]);
+                break;
+              }
+          }
+          
+        }
+
+        let timeIndex;
+        if(commandData.command === 'specifyAgenda'){
+          for(let i in this.confData.data.schedule[0].groups){
+            timeIndex = this.confData.data.schedule[0].groups[i].sessions.findIndex(x => x.timeStart.substring(0,4) === `${commandData.time.substring(0,4)}`);
+              if(timeIndex != -1){
+                console.log(timeIndex);
+                this.alanBtnComponent.nativeElement.setVisualState({agendaObject: this.confData.data.schedule[0].groups[i].sessions[timeIndex]});
+                break;
+              }
+          }
+          if(timeIndex < 0){
+            this.alanBtnComponent.nativeElement.setVisualState({agendaObject: ''});
+          }
+        }
+
+        let animalIndex;
+        if(commandData.command === 'aboutAnimal'){
+            animalIndex = this.confData.data.speakers.findIndex(x => x.name.toLowerCase() === `${commandData.animal.toLowerCase()}`);
+              if(animalIndex > -1){
+                this.alanBtnComponent.nativeElement.setVisualState({animalInfo: this.confData.data.speakers[animalIndex]});
+          }
+        }
+    });
+}
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { FormsModule } from '@angular/forms';
   ],
   declarations: [AppComponent],
   providers: [InAppBrowser, SplashScreen, StatusBar],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
